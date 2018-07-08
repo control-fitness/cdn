5fcd8e2acf02398ef8dc5d8b447df32d48474a2d
Paulo McNally <paulomcnally@gmail.com>
2018-07-08 00:08:32 -0600

    Fix process.env.PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 /**
  * Helmet can help protect your app from some well-known web vulnerabilities
