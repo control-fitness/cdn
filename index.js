d8ece15f656eef8eb92638b049d5bddea58e6ab9
Paulo McNally <paulomcnally@gmail.com>
2018-09-26 21:07:27 -0600

    Use cors package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,14 +11,30 @@ const port = process.env.PORT || 3000;
  * https://expressjs.com/en/advanced/best-practice-security.html
  * @type {[type]}
  */
-app.use(helmet.cors({
-  allowFrom: [
-    'http://*.control.fitness',
-    'http://localhost:8080'
-  ],
-  credentials: 'basic',
-  maxAge: 86400000,
-}));
+app.use(helmet());
+
+/**
+ * CORS is a node.js package for providing a Connect/Express middleware
+ * that can be used to enable CORS with various options.
+ *
+ * https://www.npmjs.com/package/cors
+ * @type {[type]}
+ */
+const whitelist = [
+  'http://*.control.fitness',
+  'https://*.control.fitness',
+  'http://localhost:8080',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+app.use(cors(corsOptions));
 
 /**
  * To serve static files such as images, CSS files, and JavaScript files,
